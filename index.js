77991d14a14b1af1fb0ce14429926df4c7567eca
Deba-hunter <106451901+Deba-hunter@users.noreply.github.com>
2025-07-27 11:34:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const {
 } = require('@whiskeysockets/baileys');
 
 const app = express();
-const PORT = process.env.PORT || 21625;
+const PORT = process.env.PORT || 3000;
 const sessionFolder = path.join(__dirname, 'session');
 
 if (!fs.existsSync(sessionFolder)) fs.mkdirSync(sessionFolder);
@@ -45,7 +45,7 @@ async function startSocket() {
     version,
     auth: state,
     printQRInTerminal: false,
-    browser: ['WhatsApp Bot', 'Chrome', '1.0'],
+    browser: ['Aadi Server', 'Chrome', '1.0'],
     getMessage: async () => ({ conversation: "hello" })
   });
 
